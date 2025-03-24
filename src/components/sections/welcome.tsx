d66c849a58e31eb5d183bce0013a4dfbdf1c4d7f
m-fadil <fadhilaikhsani@yahoo.com>
2025-03-24 20:16:49 +0700

    refactor: Move Welcome component to sections folder

--- src/components/sections/welcome.tsx
+++ src/components/sections/welcome.tsx
@@ -1,9 +1,9 @@
-import { Section } from "./section";
+import { Section } from "../section";
 import resume from "@/data/resume.json"
 
 export function Welcome() {
   return (
-		<Section className="my-48">
+		<Section className="mt-44">
 			<h5 className="text-xl font-[family-name:var(--font-geist-mono)] text-sky-400">Hi there! My name is</h5>
 			<h1 className="text-7xl font-bold">
 				Mukhammad
