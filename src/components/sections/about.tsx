4519641e64cdb424eb36d45718f5c32231ded00d
m-fadil <fadhilaikhsani@yahoo.com>
2025-03-24 20:16:41 +0700

    refactor: Move About component to sections folder

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -1,9 +1,9 @@
-import { Section } from "./section";
-import resume from "../data/resume.json";
+import { Section } from "../section";
+import resume from "../../data/resume.json";
 
 export function About() {
   return (
-		<Section>
+		<Section className="mt-24">
 			<h1 className="text-7xl font-bold">About me</h1>
 			{resume.about.map((item, index) => (
 				<p key={index} className="font-[family-name:var(--font-geist-mono)] text-pretty">
